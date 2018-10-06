bbcd6e7ed1ec5910f411cafe68a3ed68ad94992e
Vivek Patel <vivek.patel@netguru.pl>
2018-10-06 20:12:14 +0200

    Add Article and Editor route to app

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Router } from '@reach/router';
+import Article from './Article';
+import Editor from './Editor';
 import Header from './Header';
 import Home from './Home';
 import Login from './Login';
@@ -33,6 +35,9 @@ class App extends Component {
           <Home path='/' />
           <Login path='/login' setCurrentUser={this.setCurrentUser} />
           <Register path='/register' setCurrentUser={this.setCurrentUser} />
+          <Article path='/article/:slug' currentUser={currentUser} />
+          <Editor path='/editor/:slug' />
+          <Editor path='/editor' />
         </Router>
       </div>
     );
