28f920a73c5c19ac1baaae52d147796d33e9284d
Vivek Patel <vivek.patel@netguru.pl>
2018-10-06 14:02:21 +0200

    App prop to Header

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import Header from './Header';
 
 class App extends Component {
+  state = {
+    currentUser: null,
+  }
+
   render() {
+    const { currentUser } = this.state;
+
     return (
       <div>
-        <Header />
+        <Header currentUser={currentUser} />
       </div>
     );
   }
