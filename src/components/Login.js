6a8d583b2f7b170328452e7c046953889cccbc5e
Vivek Patel <vivek.patel@netguru.pl>
2018-10-06 17:57:52 +0200

    Add login components barebones

---
+++ src/components/Login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Link } from '@reach/router';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  }
+
+  render() {
+    return (
+      <div className="auth-page">
+        <div className="container page">
+          <div className="row">
+            <div className="col-md-6 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Sign In</h1>
+              <p className="text-xs-center">
+                <Link to="/register">Need an account?</Link>
+              </p>
+              <form onSubmit={this.submitForm}>
+                <fieldset>
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
+                      type="email"
+                      placeholder="Email"
+                    />
+                  </fieldset>
+                  <fieldset className="form-group">
+                    <input
+                      className="form-control form-control-lg"
+                      type="password"
+                      placeholder="Password"
+                    />
+                  </fieldset>
+                  <button
+                    className="btn btn-lg btn-primary pull-xs-right"
+                    type="submit"
+                  >
+                    Sign in
+                  </button>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
